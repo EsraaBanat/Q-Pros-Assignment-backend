9c6407328f3197cdbe12cad1121c564d6dfc722b
EsraaBanat <esraa.bnt96@gmail.com>
2023-12-12 10:44:51 +0200

    Add host name for railway deployment

--- src/main.ts
+++ src/main.ts
@@ -2,11 +2,13 @@ import { ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
+const port = process.env.PORT || 3000;
+
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
   app.useGlobalPipes(new ValidationPipe());
 
-  await app.listen(3000);
+  await app.listen(port, '0.0.0.0');
 }
 bootstrap();
