354748ddbd8e7c9a7010ddb0bc9955a278ac0c35
EsraaBanat <esraa.bnt96@gmail.com>
2023-12-12 09:46:34 +0200

    Add login api

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,5 +1,6 @@
-import { Body, Controller, Post } from '@nestjs/common';
+import { Body, Controller, Get, Post } from '@nestjs/common';
 import { AuthService } from './auth.service';
+import { LoginDto } from './dto/login.dto';
 import { SignUpDto } from './dto/signup.dto';
 
 @Controller('auth')
@@ -9,5 +10,10 @@ export class AuthController {
   @Post('/signup')
   signUp(@Body() signUpDto: SignUpDto): Promise<{ token: string }> {
     return this.authService.signUp(signUpDto);
+  }
+
+  @Get('/login')
+  login(@Body() loginDto: LoginDto): Promise<{ token: string }> {
+    return this.authService.login(loginDto);
   }
 }
