92bcca80fd6d8c864b0316577a858482ea882d24
EsraaBanat <esraa.bnt96@gmail.com>
2023-12-21 13:19:06 +0000

    Edit error messages

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -17,41 +17,51 @@ export class AuthService {
   ) {}
 
   async signUp(signUpDto: SignUpDto): Promise<{ token: string }> {
-    const { name, email, password } = signUpDto;
+    try {
+      const { name, email, password } = signUpDto;
 
-    const hashedPassword = await bcrypt.hash(password, 10);
+      const hashedPassword = await bcrypt.hash(password, 10);
 
-    const user = await this.userModel.create({
-      name,
-      email,
-      password: hashedPassword,
-    });
+      const user = await this.userModel.create({
+        name,
+        email,
+        password: hashedPassword,
+      });
 
-    const token = this.jwtService.sign({
-      id: user._id,
-      name: user.name,
-      email: user.email,
-    });
-    return { token };
+      const token = this.jwtService.sign({
+        id: user._id,
+        name: user.name,
+        email: user.email,
+      });
+      return { token };
+    } catch (error) {
+      throw error;
+    }
   }
 
   async login(loginDto: LoginDto): Promise<{ token: string }> {
-    const { email, password } = loginDto;
+    try {
+      const { email, password } = loginDto;
 
-    const user = await this.userModel.findOne({ email });
+      const user = await this.userModel.findOne({ email });
 
-    if (!user) {
-      throw new UnauthorizedException('Invalid email or password');
+      if (!user) {
+        throw new UnauthorizedException(
+          'User is not registered or the email is incorrect',
+        );
+      }
+
+      const isPasswordMatched = await bcrypt.compare(password, user.password);
+
+      if (!isPasswordMatched) {
+        throw new UnauthorizedException('Incorrect password');
+      }
+
+      const token = this.jwtService.sign({ id: user._id });
+
+      return { token };
+    } catch (error) {
+      throw error;
     }
-
-    const isPasswordMatched = await bcrypt.compare(password, user.password);
-
-    if (!isPasswordMatched) {
-      throw new UnauthorizedException('Invalid email or password');
-    }
-
-    const token = this.jwtService.sign({ id: user._id });
-
-    return { token };
   }
 }
